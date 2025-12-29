6ef0c47679ca7dfacd8b9ecc161872896425430d
rubens <rubensfqc@gmail.com>
2025-12-29 07:26:05 +0100

    minor changes

--- website/views.py
+++ website/views.py
@@ -4,3 +4,5 @@ def home(r): return render(r,'website/home.html')
 def features(r): return render(r,'website/features.html')
 def pricing(r): return render(r,'website/pricing.html')
 def contact(r): return render(r,'website/contact.html')
+
+#abas a serem criadas: inicio produtos planos blog contato empresa/atuacao
